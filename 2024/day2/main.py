20b102df5f510039c3921ddee9693bdcf6cf3687
Leo le Douarec <leo.alex@le.douar.ec>
2024-12-03 00:14:41 +0100

    day2 part2 (nice edges)

--- 2024/day2/main.py
+++ 2024/day2/main.py
@@ -143,11 +143,86 @@ def part1(matrix):
     return nb_of_safe_reports
 
 
+def part2(matrix):
+
+    nb_of_safe_reports = 0
+
+    for l in matrix:
+        safe1,safe2, safe3=False, False, False
+        
+        safe,ind = check_report_safety(l)
+
+
+        if not safe:
+            safe1,_ = check_report_safety(l[:ind] + l[ind+1:])
+            safe2,_ = check_report_safety(l[:ind-1] + l[ind:]) #ca peut etre le premier elt qui pose pb.
+            if ind >= 2:
+                safe3,_ = check_report_safety(l[:ind-2] + l[ind-1:]) #si courbe en cloche
+
+            if not (safe1 or safe2 or safe3):
+                print(l)
+                print(l[:ind] + l[ind+1:])
+                print(l[:ind-1] + l[ind:])
+                print(ind)
+            
+        
+        
+        if safe or (safe1 or safe2 or safe3):
+            nb_of_safe_reports += 1
+
+    return nb_of_safe_reports
+
+def check_report_safety(report):
+    n = len(report)
+
+    last_elt, new_elt = report[0], report[1]
+    safe=True
+    monotony_has_changed = False
+    count=1
+
+    while safe and count < n:
+            
+            #tous les cas de figure qui peuvent faire ue ca n'est plus safe:
+            # 1- 2 elts successifs sont egaux
+            # 2- on est decroissants au debut mais a un moment on devient croissant
+            # 3- on est croissants au debut mais a un moment on devient decroissant
+            # 4- le saut entre deux gusses est > 3
+
+
+            if last_elt == new_elt: safe = False
+
+            if abs(last_elt  - new_elt) > 3: safe = False
+
+            elif last_elt < new_elt: #increasing
+                if count == 1:
+                    monotony_has_changed = True
+
+                elif not monotony_has_changed: # decreases then increases
+                    safe = False #passe le premier coup, si monotony has not changed et ue on est croissants, alors c pas bon.
+
+            elif last_elt > new_elt: #increases then decreases
+                if monotony_has_changed: 
+                    safe = False
+
+
+            count += 1
+
+            if count < n - 1:
+                new_elt, last_elt = report[count], new_elt
+            else:
+                new_elt, last_elt = report[-1], new_elt # cas de bordure a deux balles !!!! berk.
+
+    return safe, count-1 #renvoyer l'indice de l'element probematique pour le pop
+
 
 
 if __name__ == "__main__":
     matrix, _ = read_input('input.txt') #j'ai trafique le fichier txt en ajoutant un retour a la ligne a la toute fin pour eviter un cas particulier ennuyant. 
 
 
-    nb_of_safe_reports = part1(matrix)
-    print(nb_of_safe_reports)
+    nb_of_safe_reports1 = part1(matrix)
+    print(nb_of_safe_reports1)
+
+
+    nb_of_safe_reports2 = part2(matrix)
+    print(nb_of_safe_reports2)
